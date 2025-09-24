c56a5669d1d0ae58a3953e096376d575b609f76a
Rey Marvin De Jesus <rmdejesus@skijapan.com>
2025-09-24 13:14:58 +0900

    changed https

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-//import mkcert from 'vite-plugin-mkcert'
+//import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [mkcert()],
   server: {
-    host: "192.168.1.57",
-    port: 9000,
-  }
+    https: true, // Enable HTTPS for the development server
+  },
 })
